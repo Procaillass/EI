3dece4f9eca6b2e6ac0a6214b3e9cd34c2fabd78
Joseph <prygieljoseph@gmail.com>
2020-02-19 14:59:00 +0100

    formulaire page contact v1

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ document.addEventListener("click", ev => {
     };
 });
 
+
+
 // APPARITION SEARCH MENU MOBILE (en cours)
 
 document.addEventListener("click", ev => {
@@ -31,3 +33,50 @@ document.addEventListener("click", ev => {
 });
 
 
+
+//// FORMULAIRE DE CONTACT
+//// LE CSS SE TROUVE DANS ---> CONTACT.SCSS
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
